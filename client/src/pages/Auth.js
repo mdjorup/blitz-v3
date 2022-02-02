a9e43db9a90a9052458bf73dde9c1787ae687557
Michael Djorup <michael.djorup@gmail.com>
2022-02-02 11:50:54 -0500

    Added initial functionality for authentication

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,20 +1,61 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../components/Button.js';
 import {SiBetfair} from 'react-icons/si'
 
+import {auth} from '../firebase.js';
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/auth';
+
+
 function Auth({type}) {
 
 
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
 
-  const handleSubmit = () => {
+  const handleRegister = () => {
+    setLoading(true)
+    setErrorMessage("");
+    if(!displayName || !email || !password) {
+      setErrorMessage("Please fill in all fields");
+      return;
+    };
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        //add user to redux state - header then needs to render right corner differently
+        //add user to db
+        setLoading(false)
+        navigate('/')
+      })
+      .catch((error) => {
+        setLoading(false)
+        setErrorMessage(error.code)
+      })
+  }
+
+  const handleLogin = () => {
+    setLoading(true)
+    setErrorMessage("")
+    if(!email || !password) {
+      setErrorMessage("Please fill in all fields");
+      return;
+    };
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        //add login time to db
+        setLoading(false)
+        navigate('/')
+      }).catch((error) => {
+        setLoading(false);
+        setErrorMessage(error.code);
+      })
 
   }
 
@@ -34,18 +75,34 @@ function Auth({type}) {
       </div>
       {/* Auth body */}
 
-      <div className='flex justify-center pt-20'>
-        <div className="flex flex-col w-1/4 rounded-xl bg-focus p-5">
+      <div className='flex justify-center pt-20 text-white'>
+        <div className="flex flex-col w-1/4 rounded-xl bg-focus p-5 text-white space-y-3.5 text-sm">
+          <div className="text-lightblue text-2xl font-semibold">
+            {type === 'register' ? "Create a Blitz Account" : "Log In to Blitz"}
+          </div>
+          {type==='register' && <div className="flex flex-col space-y-1">
+            <label>Display Name</label>
+            <input className='outline-0 bg-focus border border-gray rounded p-2 hover:border-lightgray focus:border-lightblue' 
+              type='text'
+              onChange={event=>setDisplayName(event.target.value)}></input>
+          </div>}
+          <div className="flex flex-col space-y-1">
+            <label>Email</label>
+            <input className='outline-0 bg-focus border border-gray rounded p-2 hover:border-lightgray focus:border-lightblue' 
+              type='email'
+              onChange={event => setEmail(event.target.value)}></input>
+          </div>
+          <div className="flex flex-col space-y-1">
+            <label>Password</label>
+            <input className='outline-0 bg-focus border border-gray rounded p-2 hover:border-lightgray focus:border-lightblue' 
+              type='password' 
+              onChange={event => setPassword(event.target.value)}></input>
+          </div>
+          <p className='text-error'>{errorMessage}</p>
 
-          {type === 'register' ? "Register" : "Login"}
-          <label classname=" outline-none focus:border-0 focus:outline-none">Username</label>
-          <input type='text'></input>
-          <label>Email</label>
-          <input type='email'></input>
-          <label>Password</label>
-          <input type='password'></input>
-          <Button className='object-center' text='Submit' type='primary'></Button>
-
+          <div className='text-white flex justify-center'>
+            <Button className='object-center' text='Submit' type='primary' onClick={type==='register' ? handleRegister : handleLogin}></Button>
+          </div>
         </div>
       </div>
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
       'backdrop': '#0F172A',
       'focus': '#1E293B',
       'white': '#F1F5F9',
-      'subtle': '#333344'
+      'gray': '#808080',
+      'lightgray': '#D3D3D3',
+      'subtle': '#333344',
+      'warning': '#FBBF24' ,
+      'error': '#DC2626',
     }
   },
   plugins: [],
